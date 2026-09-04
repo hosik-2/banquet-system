5f28ba898bc6ced3a071613e28cd5b283508a48a
hosik-2 <zxvbcd123@gmail.com>
2026-09-04 12:06:34 +0900

    chore: 개발용 seed 데이터 분리

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,23 +1,4 @@
-CREATE TABLE banquet(
-    banquet_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    banquet_name VARCHAR(255) NOT NULL,
-    banquet_date DATE NOT NULL,
-    start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
-    promoter_id BIGINT NOT NULL,
-    in_charge_id BIGINT,
-    venue VARCHAR(255) NOT NULL,
-    guarantee INT
-
-);
-
-CREATE TABLE member(
-    member_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    member_name VARCHAR(255) NOT NULL,
-    department VARCHAR(255) NOT NULL
-);
-
-INSERT INTO member(member_name, department)
-VALUES ('testPromoter', 'Convention');
-INSERT INTO member(member_name, department)
-VALUES ('testInCharge', 'SVC');
+INSERT INTO member(member_name, department, role)
+VALUES ('testPromoter', 'Convention', 'PROMOTER');
+INSERT INTO member(member_name, department, role)
+VALUES ('testInCharge', 'SVC', 'STAFF');
